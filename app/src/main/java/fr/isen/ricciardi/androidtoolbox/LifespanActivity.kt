939992e480905832de69cfdba2255c68c4ca2b35
FlorentRicciardi <florent.ricciardi@isen.yncrea.fr>
2020-01-21 09:10:03 +0100

    show lifecycle of the lifespan activity

--- app/src/main/java/fr/isen/ricciardi/androidtoolbox/LifespanActivity.kt
+++ app/src/main/java/fr/isen/ricciardi/androidtoolbox/LifespanActivity.kt
@@ -2,11 +2,52 @@ package fr.isen.ricciardi.androidtoolbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_lifespan.*
 
 class LifespanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lifespan)
+
+        textStateActivity.text = "onCreate"
+    }
+
+    override fun onStart(){
+        super.onStart()
+        textStateActivity.text = "onStart"
+        Log.d("TAG", "onStart")
+
+
+    }
+
+    override fun onResume(){
+        super.onResume()
+        textStateActivity.text = "onResume"
+
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        textStateActivity.text = "onRestart"
+
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("TAG", "onPause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d("TAG", "onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT)
     }
 }
